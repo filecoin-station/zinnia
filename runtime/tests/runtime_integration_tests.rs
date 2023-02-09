57c30e1037dd01855149644fa669a7b972cf0dd0
Miroslav Bajtoš <oss@bajtos.net>
2023-02-09 17:32:55 +0100

    feat: add `deno_web` APIs like `setTimeout` (#34)
    Add JS APIs from `deno_web` crate.
    
    Most notably:
    
    - Events and `AbortController`
    - `TextEncoder` and `TextDecoder` (will be needed for WASM)
    - `File`, `FileReader` and `Blob`
    - `MessageChannel`, `structuredClone` and friends
    - `atob` and `atoa` (will be needed for WASM)
    - `setTimeout`, `setInterval`, and friends
    - `Performance` APIs
    - Streams (`ReadableStream`, `WritableStream`, etc.)
    - `URL` and friends
    
    ---------
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

---
+++ runtime/tests/runtime_integration_tests.rs
@@ -0,0 +1,8 @@
+// ZINNIA VERSION: Copyright 2023 Protocol Labs. All rights reserved. MIT OR Apache-2.0 license.
+// ORIGINAL WORK: Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
+// https://github.com/denoland/deno/blob/19543ffec300390f20b577b29d92a58540aaef70/cli/tests/integration_tests.rs
+
+// The tests exist in a sub folder instead of as separate files in
+// this directory so that cargo doesn't compile each file as a new crate.
+
+mod integration;
