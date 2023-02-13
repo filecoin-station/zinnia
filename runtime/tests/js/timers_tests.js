d023ffa96d4454c6e0d28d0f1c94612d0ac09e9d
Miroslav Bajtoš <oss@bajtos.net>
2023-02-13 12:11:52 +0100

    fixup! code cleanup in timers_tests
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- runtime/tests/js/timers_tests.js
+++ runtime/tests/js/timers_tests.js
@@ -1,13 +1,13 @@
 const start = Date.now();
 
-await new Promise((resolve, reject) => {
+await new Promise((resolve) => {
   setTimeout(resolve, 50);
 });
 
-const d = Date.now() - start;
+const duration = Date.now() - start;
 
-if (d < 40 || d > 60) {
+if (duration < 40 || duration > 70) {
   throw new Error(
-    `setTimeout(50) should take between 40 to 60 ms to execute, but took ${d} ms instead`,
+    `setTimeout(50) should take between 40 to 70 ms to execute, but took ${duration} ms instead`,
   );
 }
