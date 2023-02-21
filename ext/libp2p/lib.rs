3989eff361effcbf400c66005bebeff2ff37370e
Miroslav Bajtoš <oss@bajtos.net>
2023-02-21 17:35:36 +0100

    feat: libp2p peer node
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

---
+++ ext/libp2p/lib.rs
@@ -0,0 +1,8 @@
+mod peer;
+
+// Next:
+// - Create Deno ops - dial, request_protocol
+// - Where to store the peer_node instance?
+// - Export a Deno extension
+// - Add this new extension to our runtime
+// - Write some JS tests
