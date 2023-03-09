865e4361c330a838ff4d2b93dd9497e6a2c4e56d
Miroslav Bajtoš <oss@bajtos.net>
2023-03-09 21:06:04 +0100

    feat: upgrade Deno core and extensions (#97)
    Upgrade Deno crates:
    - `deno_core` from `0.171.0` to `0.173.0`
    - `deno_runtime` from `0.97.0` to `0.99.0`
    
    Rework our internal modules to use the new ES module style.
    
    Rework the way how we call async ops to accommodate changes
    made by Deno.
    
    Disable Windows support for the time being because GitHub Actions
    Windows runners don't have enough disk space for building Zinnia.
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- runtime/js/99_main.js
+++ runtime/js/99_main.js
@@ -1,8 +1,6 @@
 // ZINNIA VERSION: Copyright 2023 Protocol Labs. All rights reserved. MIT OR Apache-2.0 license.
 // ORIGINAL WORK: Copyright 2018-2023 the Deno authors. All rights reserved. MIT license.
-// https://github.com/denoland/deno/blob/34bfa2cb2c1f0f74a94ced8fc164e81cc91cb9f4/runtime/js/99_main.js
-
-"use strict";
+// https://github.com/denoland/deno/blob/86785f21194460d713276dca2/runtime/js/99_main.js
 
 // Removes the `__proto__` for security reasons.
 // https://tc39.es/ecma262/#sec-get-object.prototype.__proto__
@@ -11,97 +9,100 @@ delete Object.prototype.__proto__;
 // Remove Intl.v8BreakIterator because it is a non-standard API.
 delete Intl.v8BreakIterator;
 
-((window) => {
-  const core = Deno.core;
-  const ops = core.ops;
-  const {
-    Error,
-    ErrorPrototype,
-    ObjectDefineProperties,
-    ObjectPrototypeIsPrototypeOf,
-    ObjectSetPrototypeOf,
-  } = window.__bootstrap.primordials;
-  const eventTarget = window.__bootstrap.eventTarget;
-  const timers = window.__bootstrap.timers;
-  const colors = window.__bootstrap.colors;
-  const inspectArgs = window.__bootstrap.console.inspectArgs;
-  const quoteString = window.__bootstrap.console.quoteString;
+const core = globalThis.Deno.core;
+const ops = core.ops;
+const primordials = globalThis.__bootstrap.primordials;
+const {
+  DateNow,
+  Error,
+  ErrorPrototype,
+  ObjectDefineProperties,
+  ObjectPrototypeIsPrototypeOf,
+  ObjectSetPrototypeOf,
+} = primordials;
+import * as util from "internal:zinnia_runtime/js/06_util.js";
+import * as event from "internal:deno_web/02_event.js";
+import * as timers from "internal:deno_web/02_timers.js";
+import * as colors from "internal:deno_console/01_colors.js";
+import { inspectArgs, quoteString, wrapConsole } from "internal:deno_console/02_console.js";
+import * as performance from "internal:deno_web/15_performance.js";
+import {
+  mainRuntimeGlobalProperties,
+  windowOrWorkerGlobalScope,
+} from "internal:zinnia_runtime/js/98_global_scope.js";
 
-  const { windowOrWorkerGlobalScope, mainRuntimeGlobalProperties } = window.__bootstrap.globalScope;
+function formatException(error) {
+  if (ObjectPrototypeIsPrototypeOf(ErrorPrototype, error)) {
+    return null;
+  } else if (typeof error == "string") {
+    return `Uncaught ${inspectArgs([quoteString(error)], {
+      colors: !colors.getNoColor(),
+    })}`;
+  } else {
+    return `Uncaught ${inspectArgs([error], { colors: !colors.getNoColor() })}`;
+  }
+}
 
-  function formatException(error) {
-    if (ObjectPrototypeIsPrototypeOf(ErrorPrototype, error)) {
-      return null;
-    } else if (typeof error == "string") {
-      return `Uncaught ${inspectArgs([quoteString(error)], {
-        colors: !colors.getNoColor(),
-      })}`;
-    } else {
-      return `Uncaught ${inspectArgs([error], {
-        colors: !colors.getNoColor(),
-      })}`;
-    }
+function runtimeStart(runtimeOptions) {
+  core.setMacrotaskCallback(timers.handleTimerMacrotask);
+  // core.setMacrotaskCallback(promiseRejectMacrotaskCallback);
+  // core.setWasmStreamingCallback(fetch.handleWasmStreaming);
+  // core.setReportExceptionCallback(event.reportException);
+  ops.op_set_format_exception_callback(formatException);
+  // version.setVersions(
+  //   runtimeOptions.denoVersion,
+  //   runtimeOptions.v8Version,
+  //   runtimeOptions.tsVersion,
+  // );
+  // build.setBuildInfo(runtimeOptions.target);
+  // util.setLogDebug(runtimeOptions.debugFlag, source);
+  colors.setNoColor(runtimeOptions.noColor || !runtimeOptions.isTty);
+  // deno-lint-ignore prefer-primordials
+  Error.prepareStackTrace = core.prepareStackTrace;
+}
+
+let hasBootstrapped = false;
+
+function bootstrapMainRuntime(runtimeOptions) {
+  if (hasBootstrapped) {
+    throw new Error("Worker runtime already bootstrapped");
   }
 
-  function runtimeStart(runtimeOptions) {
-    core.setMacrotaskCallback(timers.handleTimerMacrotask);
-    // core.setMacrotaskCallback(promiseRejectMacrotaskCallback);
-    // core.setWasmStreamingCallback(fetch.handleWasmStreaming);
-    // core.setReportExceptionCallback(reportException);
-    ops.op_set_format_exception_callback(formatException);
-    // version.setVersions(
-    //   runtimeOptions.denoVersion,
-    //   runtimeOptions.v8Version,
-    //   runtimeOptions.tsVersion
-    // );
-    // build.setBuildInfo(runtimeOptions.target);
-    // util.setLogDebug(runtimeOptions.debugFlag, source);
-    colors.setNoColor(runtimeOptions.noColor || !runtimeOptions.isTty);
-    // deno-lint-ignore prefer-primordials
-    Error.prepareStackTrace = core.prepareStackTrace;
-    // registerErrors();
+  performance.setTimeOrigin(DateNow());
+
+  const consoleFromV8 = globalThis.Deno.core.console;
+
+  // Remove bootstrapping data from the global scope
+  delete globalThis.__bootstrap;
+  delete globalThis.bootstrap;
+  util.log("bootstrapMainRuntime");
+  hasBootstrapped = true;
+
+  ObjectDefineProperties(globalThis, windowOrWorkerGlobalScope);
+  ObjectDefineProperties(globalThis, mainRuntimeGlobalProperties);
+  ObjectSetPrototypeOf(globalThis, Window.prototype);
+
+  if (runtimeOptions.inspectFlag) {
+    const consoleFromDeno = globalThis.console;
+    wrapConsole(consoleFromDeno, consoleFromV8);
   }
 
-  let hasBootstrapped = false;
+  event.setEventTargetData(globalThis);
+  event.saveGlobalThisReference(globalThis);
 
-  function bootstrapMainRuntime(runtimeOptions) {
-    if (hasBootstrapped) {
-      throw new Error("Worker runtime already bootstrapped");
-    }
+  runtimeStart(runtimeOptions);
 
-    core.initializeAsyncOps();
+  // delete `Deno` global
+  delete globalThis.Deno;
 
-    const consoleFromV8 = window.Deno.core.console;
-    const wrapConsole = window.__bootstrap.console.wrapConsole;
+  util.log("args", runtimeOptions.args);
+}
 
-    // Remove bootstrapping data from the global scope
-    delete globalThis.__bootstrap;
-    delete globalThis.bootstrap;
-    hasBootstrapped = true;
-
-    ObjectDefineProperties(globalThis, windowOrWorkerGlobalScope);
-    ObjectDefineProperties(globalThis, mainRuntimeGlobalProperties);
-    ObjectSetPrototypeOf(globalThis, Window.prototype);
-
-    if (runtimeOptions.inspectFlag) {
-      const consoleFromDeno = globalThis.console;
-      wrapConsole(consoleFromDeno, consoleFromV8);
-    }
-
-    eventTarget.setEventTargetData(globalThis);
-
-    runtimeStart(runtimeOptions);
-
-    // delete `Deno` global
-    delete globalThis.Deno;
-  }
-
-  ObjectDefineProperties(globalThis, {
-    bootstrap: {
-      value: {
-        mainRuntime: bootstrapMainRuntime,
-      },
-      configurable: true,
+ObjectDefineProperties(globalThis, {
+  bootstrap: {
+    value: {
+      mainRuntime: bootstrapMainRuntime,
     },
-  });
-})(this);
+    configurable: true,
+  },
+});
