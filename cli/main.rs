6f828876e0ecea5b40e4979d056f06f1d961aa9e
Miroslav Bajtoš <oss@bajtos.net>
2023-03-16 09:24:00 +0100

    feat: upgrade Deno to 2023.03.16 (#110)
    deno_core: 0.175.0
    deno_console: 0.93.0
    deno_crypto: 0.106.0
    deno_crypto: 0.107.0
    deno_fetch: 0.117.0
    deno_url: 0.93.0
    deno_web: 0.124.0
    deno_webidl: 0.93.0
    
    Release notes:
    https://github.com/denoland/deno/releases/tag/v1.31.3
    
    Notable changes:
    
    "Upgrades bytes crate from =1.2.1 to ^1.4.0"
    (https://github.com/denoland/deno/pull/18123)
    removes another dependency version conflict preventing us from upgrading
    libp2p to a newer version
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- cli/main.rs
+++ cli/main.rs
@@ -3,12 +3,10 @@ mod args;
 use args::{CliArgs, Commands};
 use clap::Parser;
 
-use zinnia_runtime::deno_core;
+use zinnia_runtime::anyhow::{Context, Error, Result};
+use zinnia_runtime::deno_core::error::JsError;
 use zinnia_runtime::fmt_errors::format_js_error;
-use zinnia_runtime::{colors, BootstrapOptions};
-use zinnia_runtime::{run_js_module, AnyError};
-
-use deno_core::error::JsError;
+use zinnia_runtime::{colors, resolve_path, run_js_module, BootstrapOptions};
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
@@ -23,11 +21,14 @@ async fn main() {
     }
 }
 
-async fn main_impl() -> Result<(), AnyError> {
+async fn main_impl() -> Result<()> {
     let cli_args = CliArgs::parse_from(std::env::args());
     match cli_args.command {
         Commands::Run { file } => {
-            let main_module = deno_core::resolve_path(&file)?;
+            let main_module = resolve_path(
+                &file,
+                &std::env::current_dir().context("unable to get current working directory")?,
+            )?;
             let config = BootstrapOptions {
                 agent_version: format!("zinnia/{}", env!("CARGO_PKG_VERSION")),
                 ..Default::default()
@@ -38,7 +39,7 @@ async fn main_impl() -> Result<(), AnyError> {
     }
 }
 
-fn exit_with_error(error: AnyError) {
+fn exit_with_error(error: Error) {
     // Inspired by unwrap_or_exit<T> from Deno's `cli/main.rs`
     // https://github.com/denoland/deno/blob/34bfa2cb2c1f0f74a94ced8fc164e81cc91cb9f4/cli/main.rs
     let mut error_string = format!("{error:?}");
