460b94d54135d232fcd2f4106535abefb97a302b
Miroslav Bajtoš <oss@bajtos.net>
2023-06-08 15:38:54 +0200

    fix clippy warning
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- runtime/tests/helpers/mod.rs
+++ runtime/tests/helpers/mod.rs
@@ -4,9 +4,8 @@ pub fn lassie_daemon() -> Arc<lassie::Daemon> {
     static LASSIE_DAEMON: OnceLock<Result<Arc<lassie::Daemon>, lassie::StartError>> =
         OnceLock::new();
 
-    let result = LASSIE_DAEMON.get_or_init(|| {
-        lassie::Daemon::start(lassie::DaemonConfig::default()).map(|d| Arc::new(d))
-    });
+    let result = LASSIE_DAEMON
+        .get_or_init(|| lassie::Daemon::start(lassie::DaemonConfig::default()).map(Arc::new));
 
     match result {
         Ok(ptr) => Arc::clone(ptr),
