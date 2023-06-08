7e4d990de01aa52fb97c6717f73c08a04cb11ea8
Miroslav Bajtoš <oss@bajtos.net>
2023-06-08 14:50:01 +0200

    fix tests + code cleanup
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- runtime/js/fetch.js
+++ runtime/js/fetch.js
@@ -1,34 +1,35 @@
 import { fetch as fetchImpl } from "ext:deno_fetch/26_fetch.js";
 import { fromInnerResponse, toInnerResponse } from "ext:deno_fetch/23_response.js";
 
-let ipfsPrefix = undefined;
+let ipfsScheme = "ipfs://";
+let ipfsBaseUrl = undefined;
 
 export function setLassieUrl(/** @type {string} */ value) {
-  ipfsPrefix = value + "ipfs/";
+  ipfsBaseUrl = value + "ipfs/";
 }
 
 function rewriteRetrievalUrl(resource) {
-  if (!ipfsPrefix) {
+  if (!ipfsBaseUrl) {
     throw new Error("Internal Zinnia error: Lassie URL was not configured.");
   }
   if (typeof resource !== "string") resource = String(resource);
-  return resource.replace(/^ipfs:\/\//, ipfsPrefix);
+  return ipfsBaseUrl + resource.slice(ipfsScheme.length);
 }
 
 export function fetch(resource, options) {
   // TODO: support other objects with stringifiers, e.g. URL
   // See https://developer.mozilla.org/en-US/docs/Web/API/fetch#parameters
-  if (typeof resource === "string" && resource.startsWith("ipfs://")) {
-    return fetchFromLassie(rewriteRetrievalUrl(resource), options, resource);
+  if (typeof resource === "string" && resource.startsWith(ipfsScheme)) {
+    return fetchFromIpfs(rewriteRetrievalUrl(resource), options, resource);
   }
 
   // TODO: support `resource` configured as fetch.Request
   // See https://developer.mozilla.org/en-US/docs/Web/API/Request
 
-  return fetchImpl(...args);
+  return fetchImpl(resource, options);
 }
 
-async function fetchFromLassie(resource, options, originalUrl) {
+async function fetchFromIpfs(resource, options) {
   // Call Deno's `fetch` using the rewritten URL to make the actual HTTP request
   const response = await fetchImpl(resource, options);
 
@@ -36,7 +37,7 @@ async function fetchFromLassie(resource, options, originalUrl) {
   // We don't want to leak Lassie's URL
   const inner = toInnerResponse(response);
   inner.urlList = inner.urlList.map((url) =>
-    url.startsWith(ipfsPrefix) ? "ipfs://" + url.slice(ipfsPrefix.length) : url,
+    url.startsWith(ipfsBaseUrl) ? "ipfs://" + url.slice(ipfsBaseUrl.length) : url,
   );
   return fromInnerResponse(inner);
 }
