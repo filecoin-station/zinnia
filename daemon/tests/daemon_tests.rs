4849bd2df242a9b5828f43d9ef6112b9cea6b8a7
Miroslav Bajtoš <oss@bajtos.net>
2023-06-22 10:19:09 +0200

    code cleanup in tests and implement the fix
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- daemon/tests/daemon_tests.rs
+++ daemon/tests/daemon_tests.rs
@@ -1,45 +1,44 @@
-use assert_fs::prelude::*;
 use std::fs::read_dir;
 use std::io::{BufRead, BufReader};
+use std::path::PathBuf;
 use std::process::{Command, Stdio};
 use std::time::Duration;
 use tempfile::tempdir;
 
 #[test]
 pub fn it_removes_lassie_temp_on_start() {
+    let _ = env_logger::builder().is_test(true).try_init();
+
     let temp_root = tempdir().expect("cannot create temporary directory");
     let cache_root = temp_root.path().join("cache");
     let state_root = temp_root.path().join("state");
 
-    let mod_js = assert_fs::NamedTempFile::new("retriever.js")
-        .expect("cannot create temporary retriever.js");
-    mod_js
-        .write_str(
-            r#"
-// Signal that we are going to start the retrieval
-Zinnia.activity.info("fetch-start");
-await fetch('ipfs://QmdmQXB2mzChmMeKY47C43LxUdg1NDJ5MWcKMKxDu7RgQm');
-// Signal that the retrieval has finished. If this happens then the test
-// did not kill the zinniad process quickly enough.
-Zinnia.activity.info("fetch-end");
-"#,
-        )
-        .expect(&format!("cannot write to {}", mod_js.display()));
+    let mut mod_js = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    mod_js.push("tests");
+    mod_js.push("fixtures");
+    mod_js.push("fetch-ipfs-with-delay.js");
+    assert!(mod_js.is_file(), "test JS file not found - {:?}", mod_js);
 
     let bin = assert_cmd::cargo::cargo_bin("zinniad");
-    let bin_str = bin.display().to_string();
-    assert!(bin.is_file(), "{} not found", bin_str);
+    assert!(bin.is_file(), "zinniad not found - {:?}", bin);
 
-    // Start zinniad in background
+    // Create a command to start zinniad
     let mut cmd = Command::new(bin);
     cmd.env("NO_COLOR", "1")
         .env("FIL_WALLET_ADDRESS", "f1test")
         .env("CACHE_ROOT", cache_root.display().to_string())
         .env("STATE_ROOT", state_root.display().to_string())
-        .args([&mod_js.path().display().to_string()])
+        .args([&mod_js.as_os_str()])
         .stdout(Stdio::piped());
 
-    let mut child = cmd.spawn().expect(&format!("cannot spawn {}", bin_str));
+    if !log::log_enabled!(log::Level::Debug) {
+        cmd.stderr(Stdio::null());
+    }
+
+    // Start zinniad in background
+    let mut child = cmd
+        .spawn()
+        .unwrap_or_else(|_| panic!("cannot spawn {:?}", cmd.get_program()));
     let mut stdout_lines = BufReader::new(child.stdout.take().expect("cannot take child's stdout"))
         .lines()
         .map(|it| it.expect("cannot read from child's stdout"))
@@ -48,7 +47,7 @@ Zinnia.activity.info("fetch-end");
     // Wait until our module starts and calls `fetch('ipfs://...')`
     stdout_lines
         .by_ref()
-        .take_while(|ln| !ln.contains("fetch-start"))
+        .take_while(|ln| !ln.contains("fetch:start"))
         .for_each(drop);
 
     // Wait until Lassie creates its temp file
@@ -70,7 +69,7 @@ Zinnia.activity.info("fetch-end");
 
     // Read the rest of stdout, ensure the retrieval was interrupted and did not finish yet
     assert!(
-        stdout_lines.find(|ln| ln.contains("fetch-end")).is_none(),
+        !stdout_lines.any(|ln| ln.contains("fetch-end")),
         "zinniad should have been killed before the retrieval finished"
     );
 
@@ -80,12 +79,14 @@ Zinnia.activity.info("fetch-end");
         .map(|it| it.expect("cannot parse directory entry").path())
         .collect();
     assert!(
-        old_files.len() > 0,
+        !old_files.is_empty(),
         "Lassie should have left some temp files"
     );
 
     // Run zinniad again
-    let mut child = cmd.spawn().expect(&format!("cannot spawn {}", bin_str));
+    let mut child = cmd
+        .spawn()
+        .unwrap_or_else(|_| panic!("cannot spawn {:?}", cmd.get_program()));
     let mut stdout_lines = BufReader::new(child.stdout.take().expect("cannot take child's stdout"))
         .lines()
         .map(|it| it.expect("cannot read from child's stdout"))
@@ -94,7 +95,7 @@ Zinnia.activity.info("fetch-end");
     // Wait until our module starts and calls `fetch('ipfs://...')`
     stdout_lines
         .by_ref()
-        .take_while(|ln| !ln.contains("fetch-start"))
+        .take_while(|ln| !ln.contains("fetch:start"))
         .for_each(drop);
 
     // Stop the process
@@ -106,7 +107,7 @@ Zinnia.activity.info("fetch-end");
     let found: Vec<_> = read_dir(cache_root.join("lassie"))
         .expect("cannot list files in Lassie's temp dir")
         .map(|it| it.expect("cannot parse directory entry").path())
-        .filter(|ent| old_files.contains(&ent))
+        .filter(|ent| old_files.contains(ent))
         .collect();
 
     assert_eq!(
