8c5528a8de74d59d900378b1b2c58539eb8949fe
Miroslav Bajtoš <oss@bajtos.net>
2024-05-13 14:52:50 +0200

    fix: use `0x00(...)0DEaD` as the dev wallet
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- runtime/tests/js/station_apis_tests.js
+++ runtime/tests/js/station_apis_tests.js
@@ -5,7 +5,7 @@ test("Zinnia.walletAddress", () => {
   // Runtime JS tests are executed with the default configuration
   // In this test, we assert that we can access the wallet address
   // and the value is the default testnet one.
-  assertStrictEquals(Zinnia.walletAddress, "t1abjxfbp274xpdqcpuaykwkfb43omjotacm2p3za");
+  assertStrictEquals(Zinnia.walletAddress, "0x000000000000000000000000000000000000dEaD");
 });
 
 test("Zinnia.stationId", () => {
