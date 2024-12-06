433c9396ec513216a5b46e64a23f22c24f430b94
Miroslav Bajtoš <oss@bajtos.net>
2024-12-06 16:42:02 +0100

    test: fix "it_removes_lassie_temp_on_start"
    Also add more diagnostics to simplify troubleshooting in the future.
    
    Here is how to run this single test and get the additional info:
    
    ```
    cargo test --test daemon_tests -- --nocapture
    ```
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- daemon/tests/fixtures/fetch-ipfs-with-delay.js
+++ daemon/tests/fixtures/fetch-ipfs-with-delay.js
@@ -1,6 +1,6 @@
 // Signal that we are going to start the retrieval
 Zinnia.activity.info("fetch:start");
-const response = await fetch("ipfs://QmdmQXB2mzChmMeKY47C43LxUdg1NDJ5MWcKMKxDu7RgQm");
+const response = await fetch("ipfs://bafybeiazvkej6ou3w6xmva5ed6suonxjv3jkhq4ke73q5hgmcjmf76uos4");
 
 Zinnia.activity.info("fetch:response-headers");
 
